18094e837c2b6838c84570def1c575b7dc00e13b
Roger Camacho <roger.camacho@mitrol.net>
2026-09-09 17:19:28 -0300

    2do ej condicionales para calificaciones

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,76 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        try
+        {
+            Console.Write("What is your grade percentage? ");
+            int percentage = int.Parse(Console.ReadLine());
+
+            if (percentage < 0 || percentage > 100)
+            {
+                Console.WriteLine("invalid input. enter between 0 and 100.");
+                return;
+            }
+
+            string letter;
+
+            if (percentage >= 90)
+            {
+                letter = "A";
+            }
+            else if (percentage >= 80)
+            {
+                letter = "B";
+            }
+            else if (percentage >= 70)
+            {
+                letter = "C";
+            }
+            else if (percentage >= 60)
+            {
+                letter = "D";
+            }
+            else
+            {
+                letter = "F";
+            }
+
+
+            int lastDigit = percentage % 10;
+            string sign;
+
+            if (letter == "A" && lastDigit < 3)
+            {
+                sign = "-";
+            }
+            else if (letter != "A" && letter != "F" && lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (letter != "A" && letter != "F" && lastDigit < 3)
+            {
+                sign = "-";
+            }
+            else
+            {
+                sign = "";
+            }
+        
+
+        Console.WriteLine($"your grade is: {letter}{sign}");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("good, you passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("next time.");
+        }
+    }
+    catch (FormatException)
+        {
+            Console.WriteLine("Invalid input. Please enter a valid integer for the grade percentage.");
+        }
     }
 }
